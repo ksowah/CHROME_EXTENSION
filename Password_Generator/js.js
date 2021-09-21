b032b688c9a1c596fa1adb80ab2ca32f6d485cba
Kevv_in <80095257+ksowah@users.noreply.github.com>
2021-09-21 00:16:04 +0000

    Add files via upload

---
+++ Password_Generator/js.js
@@ -0,0 +1,112 @@
+const pwEl = document.getElementById("pw")
+const copyEl = document.getElementById("copy")
+const lenEl = document.getElementById("len")
+const upperEl = document.getElementById("upper")
+const lowerEl = document.getElementById("lower")
+const numberEl = document.getElementById("number")
+const symbolEl = document.getElementById("symbol")
+const generateEl = document.getElementById("generate")
+
+const upperLetters = "QWERTYUIOPLKJHGFDSAZXCVBNM" 
+const lowerLetters = "qwertyuioplkjhgfdsazxcvbnm"
+const numbers = "0123456789"
+const symbols = "!@#$%^&*()_+`?>*"
+
+function getUppercase(){
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)]
+}
+
+function getLowercase(){
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
+
+}
+
+function getNumber(){
+    return numbers[Math.floor(Math.random() * numbers.length)]
+
+}
+
+function getSymbols(){
+    return symbols[Math.floor(Math.random() * symbols.length)]
+
+}
+
+
+function generatePassword(){
+    let password = ''
+    const len = lenEl.value
+
+    if(upperEl.checked){
+       password += getUppercase()
+    }
+
+    if(lowerEl.checked){
+       password += getLowercase()
+    }
+
+    if(number.checked){
+        password += getNumber()
+    }
+
+    if(symbolEl.checked){
+       password += getSymbols()
+    }
+
+    if(password.length > lenEl.value) return ""
+
+    if(lenEl.value > 40){
+        alert("You have exceeded a maximum of 40 characters")
+        return ""
+    }
+
+
+    for(let i = password.length; i < len ; i++){
+        const x = generateX()
+        password += x
+    }
+
+    pwEl.innerText = password
+}
+
+function generateX(){
+    const xs = []
+    if(upperEl.checked){
+        xs.push(getUppercase())
+    }
+
+    if(lowerEl.checked){
+        xs.push(getLowercase())
+    }
+
+    if(number.checked){
+        xs.push(getNumber())
+    }
+
+    if(symbolEl.checked){
+        xs.push(getSymbols())
+    }
+
+    if(xs.length === 0){
+        return ""
+    }
+
+    return xs[Math.floor(Math.random() * xs.length)]
+}
+
+generateEl.addEventListener("click", generatePassword)
+
+copyEl.addEventListener("click", () => {
+    const textarea = document.createElement("textarea")
+    const password = pwEl.innerText
+
+    if(!password){
+        return
+    }
+
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand("copy")
+    textarea.remove()
+    alert("Password copied to clipboard")
+})
